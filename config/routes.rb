b9b1450f2af30151b361e3eaeeb4288b390d3e1f
Kaylie Gonzalez <kaylieag@tamu.edu>
2021-10-02 17:22:28 -0500

    announcements routes working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :announcements
   resources :videos
+  root 'home#index'
   get 'home/index'
+  get '/Announcements', to: 'announcements#index' 
   resources :affiliate_orgs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
