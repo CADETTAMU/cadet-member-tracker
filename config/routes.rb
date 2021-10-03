30431204439a6b9d485d915989819f8f19b77976
rpickett0173 <ryan.pickett.w@tamu.edu>
2021-10-03 14:49:46 -0500

    remove public files

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :videos
   root 'home#index'
   get 'home/index'
-  get '/Announcements', to: 'announcements#index' 
+  get 'videos/index'
+  get 'Caldendar.html'
+  get '/Announcements', to: 'announcements#index'
   resources :affiliate_orgs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
