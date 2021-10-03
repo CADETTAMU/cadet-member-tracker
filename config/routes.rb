8662cf47925b507ca9b7e9af008ec93a0ceb5f92
Kaylie Gonzalez <kaylieag@tamu.edu>
2021-10-03 16:50:06 -0500

    video routes working

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   resources :videos
   root 'home#index'
   get 'home/index'
-  get 'videos/index'
+  get '/Videos', to: 'videos#index'
   get '/Calendar', to: 'home#calendar'
   get '/Announcements', to: 'announcements#index'
+  get '/Home', to: 'home#index'
   resources :affiliate_orgs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
