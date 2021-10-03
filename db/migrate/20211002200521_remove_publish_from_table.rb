beccfc4a53232d2b54dec2a6410df824e1a9cc78
rpickett0173 <ryan.pickett.w@tamu.edu>
2021-10-03 16:31:47 -0500

    removing migrate

--- db/migrate/20211002200521_remove_publish_from_table.rb
+++
@@ -1,5 +0,0 @@
-class RemovePublishFromTable < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :announcements, :publish_id
-  end
-end
--- db/migrate/20211002201849_remove_publish_id_from_table.rb
+++
@@ -1,5 +0,0 @@
-class RemovePublishIdFromTable < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :videos, :publish_id
-  end
-end
