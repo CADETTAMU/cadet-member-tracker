1db652a1b014ca6a170d311d9af4dfa54bf9fda9
Kaylie Gonzalez <kaylieag@tamu.edu>
2021-10-03 16:03:32 -0500

    announcements now routes back correctly after show

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   get '/Announcements', to: 'announcements#index' 
-  resources :affiliate_orgs
+  resources :announcements do
+    collection do
+      get 'announcements'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
