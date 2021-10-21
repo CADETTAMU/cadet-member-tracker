d44d6558ba011411878d389f4ccd1b2e80516053
rpickett0173 <ryan.pickett.w@tamu.edu>
2021-10-20 20:28:23 -0500

    CRUD afil

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
     get 'members/sign_in', to: 'members/sessions#new', as: :new_member_session
 	get 'members/sign_out', to: 'sign_in#index', as: :destroy_member_session
   end
-  
+
   resources :attendances
   resources :announcements
   resources :videos
+  resources :affiliate_orgs
   root 'home#index'
   get 'home/index'
   get '/Videos', to: 'videos#index'
@@ -16,6 +17,6 @@ Rails.application.routes.draw do
   get '/Attendances', to: 'attendances#index'
   get '/Home', to: 'home#index'
   get '/Sign_In', to: 'sign_in#index'
-  resources :affiliate_orgs
+  get '/Links to Affiliate Orgs', to: 'affiliate_orgs#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
