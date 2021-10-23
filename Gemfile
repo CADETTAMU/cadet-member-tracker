c57ce2c218aed94af71ce2c3f5620189db271674
astoner3 <amandaestoner@gmail.com>
2021-10-23 13:56:58 -0500

    sign in restrictions
    only display link to attendance tracker on all working pages if a member is signed in

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'omniauth'
 gem 'omniauth-google-oauth2'
 gem "omniauth-rails_csrf_protection"
 
+# Youtube gems
+gem 'youtube_addy'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_211539) do
+ActiveRecord::Schema.define(version: 2021_10_22_235953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,13 +50,25 @@ ActiveRecord::Schema.define(version: 2021_10_22_211539) do
     t.index ["email"], name: "index_members_on_email", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   create_table "videos", force: :cascade do |t|
     t.datetime "publish_date"
     t.string "title"
-    t.string "link"
     t.string "uploader"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "link"
   end
 
 end
