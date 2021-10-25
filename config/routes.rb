c882d57db20e4369d200e7d480fe101519ed74c1
rpickett0173 <ryan.pickett.w@tamu.edu>
2021-10-24 21:06:43 -0500

    updated html

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,12 @@ Rails.application.routes.draw do
   resources :affiliate_orgs
   root 'home#index'
   get 'home/index'
+  get '/Home', to: 'home#index'
   get '/Videos', to: 'videos#index'
   get '/Calendar', to: 'home#calendar'
   get '/Announcements', to: 'announcements#index'
   get '/Attendances', to: 'attendances#index'
-  get '/Home', to: 'home#index'
-  get '/Sign_In', to: 'sign_in#index'
   get '/Links to Affiliate Orgs', to: 'affiliate_orgs#index'
+  get '/Sign_In', to: 'sign_in#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
