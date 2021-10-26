e6f772d233a512d824e9c5d82d970779aafca5a2
Kaylie Gonzalez <kaylieag@tamu.edu>
2021-10-25 20:59:03 -0500

    rspec testing not working with admin

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -2,11 +2,46 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an announcement', type: :feature do
-  scenario 'valid inputs' do
-    visit new_book_path
-    fill_in 'Title', with: 'harry potter'
-    click_on 'Create Book'
-    visit books_path
-    expect(page).to have_content('harry potter')
+  scenario 'valid title' do
+    visit new_announcement_path
+    fill_in 'title', with: 'test announcement title'
+    fill_in 'body', with: 'test announcement body'
+    select '2020', :from => 'announcement_published_date_1i'
+    select 'January', :from => 'announcement_published_date_2i'
+    select '10', :from => 'announcement_published_date_3i'
+    select '10', :from => 'announcement_published_date_4i'
+    select '10', :from => 'announcement_published_date_5i'
+    click_on 'Create Announcement'
+    visit announcements_path
+    expect(page).to have_content('test announcement title')
   end
+  
+  scenario 'valid body' do
+    visit new_announcement_path
+    fill_in 'title', with: 'test announcement title'
+    fill_in 'body', with: 'test announcement body'
+    select '2020', :from => 'announcement_published_date_1i'
+    select 'January', :from => 'announcement_published_date_2i'
+    select '10', :from => 'announcement_published_date_3i'
+    select '10', :from => 'announcement_published_date_4i'
+    select '10', :from => 'announcement_published_date_5i'
+    click_on 'Create Announcement'
+    visit announcements_path
+    expect(page).to have_content('test announcement body')
+  end
+
+  scenario 'valid inputs' do
+    visit new_announcement_path
+    fill_in 'title', with: 'test announcement title'
+    fill_in 'body', with: 'test announcement body'
+    select '2020', :from => 'announcement_published_date_1i'
+    select 'January', :from => 'announcement_published_date_2i'
+    select '10', :from => 'announcement_published_date_3i'
+    select '10', :from => 'announcement_published_date_4i'
+    select '10', :from => 'announcement_published_date_5i'
+    click_on 'Create Announcement'
+    visit announcements_path
+    expect(page).to have_content('2020-01-10 10:10')
+  end
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,18 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  module SpecTestHelper   
+    def login_admin
+      login(:admin)
+    end
+  
+    def login(user)
+      user = User.where(:login => user.to_s).first if user.is_a?(Symbol)
+      request.session[:user] = user.id
+    end
+  
+    def current_user
+      User.find(request.session[:user])
+    end
+  end
 end
