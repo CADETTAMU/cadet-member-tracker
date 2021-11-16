8c88fd5b6508f5943884c7f0e1f82e771685b15c
mitribasseltoubbeh <mitribasseltoubbeh@tamu.edu>
2021-11-15 19:25:04 -0600

    Fixed routing issue
    Acceptance Criteria: Users can access other points from the sign in/profile page.
    
    Definition of Done: Routing issue no longer occurs after different actions take place on sign in/profile.
    
    Coding standard: Better route logic.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # For OAuth
   devise_for :members, controllers: {omniauth_callbacks: 'members/omniauth_callbacks'}
   devise_scope :member do
-    get 'members/sign_in', to: 'sign_in#index', as: :new_member_session
-	get 'members/sign_out', to: 'devise/sessions#destroy', as: :destroy_member_session
+    get '/sign_in', to: 'sign_in#index', as: :new_member_session
+	get '/sign_out', to: 'devise/sessions#destroy', as: :destroy_member_session
   end
   
   # For OAuth Testing
